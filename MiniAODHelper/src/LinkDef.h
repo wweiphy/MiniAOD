018e50f0490733d4ed4b8f3eb635ac9b9b2b006e
Matthias Wolf <matthias@sushinara.net>
2015-11-05 07:18:30 -0500

    Add code for csv weights.
    Taken from the TWiki and “classified.”

---
+++ MiniAODHelper/src/LinkDef.h
@@ -0,0 +1,15 @@
+#include "MiniAOD/MiniAODHelper/interface/CSVHelper.h"
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all structs;
+#pragma link off all functions;
+
+#pragma link C++ nestedclasses;
+#pragma link C++ nestedtypedefs;
+
+#pragma link C++ class CSVHelper+;
+
+#endif
